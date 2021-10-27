6d1c3351282d5986bbdc8e8a35c748e108a5c49b
Cesar Barrero <32602779+CeBarrero@users.noreply.github.com>
2021-10-26 20:54:25 -0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 class Database{
     public static function connect() {
         
-        $db =  new mysqli('bswt27gmvho2ck4gf2ld-mysql.services.clever-cloud.com', 'udtovgzdu0xww6f2', 'udtovgzdu0xww6f2', 'bswt27gmvho2ck4gf2ld');
+        $db =  new mysqli('localhost', 'root', '', 'tienda_master');
         $db->query("SET NAMES 'utf8'");
         return $db;
     }
