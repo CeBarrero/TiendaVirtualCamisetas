c0d038852b87b030da4009c43748fe03c499e380
CeBarrero <barreroce@gmail.com>
2021-10-26 20:17:29 -0500

    Primer commit

---
+++ views/layout/footer.php
@@ -0,0 +1,10 @@
+                </div>            
+            </div>
+
+            <!-- PIE DE PAGINA -->
+            <footer id="footer">
+                <p>Desarrollado por Cristian y Cesar Barrero &copy; <?= date('Y') ?></p>
+            </footer>
+        </div>
+    </body>
+</html>
